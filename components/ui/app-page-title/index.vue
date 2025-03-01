a5c4813ac9d121d03ea7c6e0ba08953d5fc2eeb7
daniel verdu <iamadeveloper.es@gmail.com>
2025-03-01 17:50:52 +0100

    Add new component app-page-title
    Component to show title and subtitle in a page

---
+++ components/ui/app-page-title/index.vue
@@ -0,0 +1,28 @@
+<!-- Giving a name for Vue extension -->
+<script lang="ts">
+export default {
+  name: 'page-title'
+};
+</script>
+
+<script lang="ts" setup>
+defineProps({
+  title: {
+    type: String
+  },
+  subtitle: {
+    type: String
+  }
+});
+</script>
+
+<template>
+  <div class="app-title-page">
+    <h1 class="uppercase font-heading-one text-heading-one">{{ title }}</h1>
+    <p v-if="subtitle" class="font-primary-light">{{ subtitle }}</p>
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
