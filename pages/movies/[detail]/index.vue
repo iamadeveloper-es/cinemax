a10935c1f5b75de405fd9c0c14735b150fcaff2a
daniel verdu <iamadeveloper.es@gmail.com>
2025-03-01 17:54:10 +0100

    Add pages for movies and tv shows details

---
+++ pages/movies/[detail]/index.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+export default {
+  name: 'movie-detail'
+};
+</script>
+
+<script lang="ts" setup>
+const route = useRoute();
+const {$api} = useNuxtApp();
+
+const movie = ref({});
+
+const getMovie = async () => {
+  const {data, error} = await $api.media.findOneById(route.params.detail);
+  movie.value = data.value;
+};
+
+getMovie();
+
+</script>
+
+<template>
+  <NuxtLayout>
+    <section class="section">
+      <div class="md:container md:mx-auto">
+        <UiAppPageTitle
+        :title="movie.Title"
+        />
+        <UiAppMediaDetail :media="movie"/>
+      </div>
+    </section>
+  </NuxtLayout>
+</template>
+
+<style>
+</style>
---
+++ pages/tv-shows/[detail]/index.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+export default {
+  name: 'tv-show-detail'
+};
+</script>
+
+<script lang="ts" setup>
+const route = useRoute();
+const {$api} = useNuxtApp();
+
+const tvShow = ref({});
+
+const getTvShow = async () => {
+  const {data, error} = await $api.media.findOneById(route.params.detail);
+  tvShow.value = data.value;
+};
+
+getTvShow();
+
+</script>
+
+<template>
+  <NuxtLayout>
+    <section class="section">
+      <div class="md:container md:mx-auto">
+        <UiAppPageTitle
+        :title="tvShow.Title"
+        />
+        <UiAppMediaDetail :media="tvShow"/>
+      </div>
+    </section>
+  </NuxtLayout>
+</template>
+
+<style>
+</style>
