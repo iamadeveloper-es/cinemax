d2eade2823aafb70624aa5fe2945434f8a2c00b7
daniel verdu <iamadeveloper.es@gmail.com>
2025-03-01 11:33:04 +0100

    Add component header

--- components/ui/app-header/index.vue
+++ components/ui/app-header/index.vue
@@ -1,9 +1,12 @@
 <template>
-  <header class="app-header">
-    <nav>
-      <ul>
+  <header class="app-header px-3 md:px-0 py-4 bg-primary-300rgba-80">
+    <nav class="flex justify-between items-center md:container md:mx-auto">
+      <NuxtLink to="/">
+        <img class="w-[30px] h-auto" src="/images/cinemax-logo.png" alt="Cinemax Logo" width="40" height="40">
+      </NuxtLink>
+      <ul class="flex justify-end gap-4">
         <li v-for="(route, index) in menuRoutes" :key="index">
-          <nuxt-link :to="route.route">{{ route.name }}</nuxt-link>
+          <NuxtLink :to="route.route" class="text-grey-200 font-primary-light">{{ route.name }}</NuxtLink>
         </li>
       </ul>
     </nav>
@@ -23,6 +26,10 @@ const menuRoutes = [
 ];
 </script>
 
-<style>
-
+<style lang="scss">
+.app-header{
+  backdrop-filter: blur(20px);
+  position: sticky;
+  top: 0;
+}
 </style>
