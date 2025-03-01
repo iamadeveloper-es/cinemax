1381fd564e0fdd046426a7e13da6a183a3af846b
daniel verdu <iamadeveloper.es@gmail.com>
2025-03-01 17:51:44 +0100

    Add component card-media that represent a media card item

---
+++ components/ui/app-card-media/index.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+export default {
+  name: 'card-media'
+};
+</script>
+
+<script lang="ts" setup>
+const emit = defineEmits(['onClick']);
+
+const props = defineProps({
+  media: {
+    type: Object,
+    required: true
+  }
+});
+
+const onClick = () => {
+  emit('onClick', props.media);
+};
+
+</script>
+
+<template>
+  <div
+    class="media-card col-span-6 sm:col-span-4 md:col-span-3 lg:col-span-2 cursor-pointer"
+    @click="onClick">
+      <img :src="media.Poster" :alt="media.Title">
+      <h4 class="media-card-title my-3">{{ media.Title }}</h4>
+  </div>
+</template>
+
+<style>
+
+</style>
