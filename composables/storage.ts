bf74c9de9d4ae71df0d77d7d15a10d2facc40610
daniel verdu <iamadeveloper.es@gmail.com>
2025-03-02 02:43:09 +0100

    Add snackbar to control when a media is added to favorite list

--- composables/storage.ts
+++ composables/storage.ts
@@ -2,7 +2,7 @@ import { useUserList } from '~/store/userList';
 
 export const useStorage = () => {
   const { addToList } = useUserList();
-
+  const snackbar = useSnackbar();
   const setToLocalStorage = (payload: { imdbID: string }) => {
     const keyName = 'viewed';
     const storagedItems = JSON.parse(localStorage.getItem(keyName) || '[]');
@@ -11,6 +11,11 @@ export const useStorage = () => {
       storagedItems.push(payload);
       localStorage.setItem(keyName, JSON.stringify(storagedItems));
       addToList(storagedItems);
+      snackbar.add({
+        type: 'success',
+        background: '#7B6EF6',
+        text: `${payload.Title} was added to your list`
+      });
     }
   };
 
